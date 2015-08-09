e7c326a57f32882653cb7c4dfe0b1032c4690c42
Meade Slocomb <meade.slocomb@gmail.com>
2015-08-09 15:09:42 -0400

    Long time without commit...added most assets, and structure. Front page and skill section

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,22 @@
-console.log('Angular Rocks', angular);
+(function() {
+  'use strict';
+
+  angular.module('MSPortfolio', ['ui.router'])
+
+
+    .config([ '$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+
+      $urlRouterProvider.otherwise('/');
+
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'js/templates/portfolio.tpl.html',
+          controller: 'PortfolioController'
+        });
+
+    }
+  ]);
+
+}());
---
+++ app/js/portfolio.controller.js
@@ -0,0 +1,94 @@
+(function (){
+
+  'use strict';
+
+  angular.module('MSPortfolio')
+
+  .controller('PortfolioController', ['$scope',
+
+    function ($scope) {
+
+      $scope.javascript = {
+        title: 'javascript',
+        path: 'javascript',
+        desc: 'JavaScript is my favorite language',
+        github: 'https://github.com/Code-Bytes'
+      };
+
+      $scope.angular = {
+        title: 'angular',
+        path: 'javascript/angular',
+        desc: 'JavaScript is my favorite language',
+        github: 'https://github.com/Code-Bytes'
+      };
+
+      $scope.backbone = {
+        title: 'backbone',
+        path: 'javascript/backbone',
+        desc: 'JavaScript is my favorite language',
+        github: 'https://github.com/Code-Bytes'
+      };
+
+      $scope.ajax = {
+        title: 'AJAX',
+        path: 'javascript/ajax',
+        desc: 'JavaScript is my favorite language',
+        github: 'https://github.com/Code-Bytes'
+      };
+
+      $scope.css = {
+        title: 'CSS3',
+        path: 'css/css3',
+        desc: 'JavaScript is my favorite language',
+        github: 'https://github.com/Code-Bytes'
+      };
+
+      $scope.sass = {
+        title: 'Sass',
+        path: 'css/sass',
+        desc: 'JavaScript is my favorite language',
+        github: 'https://github.com/Code-Bytes'
+      };
+
+      $scope.foundation = {
+        title: 'foundation',
+        path: 'css/foundation',
+        desc: 'JavaScript is my favorite language',
+        github: 'https://github.com/Code-Bytes'
+      };
+
+      $scope.bourbon = {
+        title: 'bourbon',
+        path: 'css/bourbon',
+        desc: 'JavaScript is my favorite language',
+        github: 'https://github.com/Code-Bytes'
+      };
+
+      $scope.html = {
+        title: 'HTML5',
+        path: 'markup/html5',
+        desc: 'JavaScript is my favorite language',
+        github: 'https://github.com/Code-Bytes'
+      };
+
+      $scope.github = {
+        title: 'github',
+        path: 'versioncontrol/github',
+        desc: 'JavaScript is my favorite language',
+        github: 'https://github.com/Code-Bytes'
+      };
+
+      $scope.displayedLang = $scope.javascript;
+
+      $scope.changeLang = function(lang){
+        $scope.displayedLang = lang;
+      };
+
+
+
+
+  }
+
+  ]);
+
+}());
